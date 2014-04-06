b375079b8115ddf1a3e3bb5a9b15c9b4610e6333
Aaron Burtnyk <burt202@gmail.com>
2014-04-06 17:07:56 +0100

    adds params example

---
+++ examples/params.php
@@ -0,0 +1,9 @@
+<?php
+
+if ($_FILES['fileUpload']['name'][0]) {
+    $uploadedUrl = 'uploads/' . $_FILES['fileUpload']['name'][0];
+    move_uploaded_file($_FILES['fileUpload']['tmp_name'][0], $uploadedUrl);
+}
+
+echo json_encode($_POST);
+exit;
