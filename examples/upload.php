7134d2f9271df7339f507babad0ac98f499c1aaa
Aaron Burtnyk <burt202@gmail.com>
2014-04-06 12:23:26 +0100

    updates existing example and adds a basic one

--- examples/upload.php
+++ examples/upload.php
@@ -1,43 +1,9 @@
 <?php
 
-$urls = array();
-
-if (isset($_POST['liteUploader_id']) && $_POST['liteUploader_id'] == 'fileUpload1')
-{
-	foreach ($_FILES['fileUpload1']['error'] as $key => $error)
-	{
-	    if ($error == UPLOAD_ERR_OK)
-		{
-			$uploadedUrl = 'uploads/' . $_FILES['fileUpload1']['name'][$key];
-	        move_uploaded_file( $_FILES['fileUpload1']['tmp_name'][$key], $uploadedUrl);
-	        $urls[] = $uploadedUrl;
-	    }
-	}
-
-	$message = 'Successfully Uploaded File(s) From First Upload Input';
+if ($_FILES['fileUpload']['name'][0]) {
+    $uploadedUrl = 'uploads/' . $_FILES['fileUpload']['name'][0];
+    move_uploaded_file($_FILES['fileUpload']['tmp_name'][0], $uploadedUrl);
 }
 
-if (isset($_POST['liteUploader_id']) && $_POST['liteUploader_id'] == 'fileUpload2')
-{
-	foreach ($_FILES['fileUpload2']['error'] as $key => $error)
-	{
-	    if ($error == UPLOAD_ERR_OK)
-		{
-			$uploadedUrl = 'uploads/' . $_FILES['fileUpload2']['name'][$key];
-	        move_uploaded_file( $_FILES['fileUpload2']['tmp_name'][$key], $uploadedUrl);
-	        $urls[] = $uploadedUrl;
-	    }
-	}
-
-	$message = 'Successfully Uploaded File(s) From Second Upload Input';
-}
-
-echo json_encode(
-	array(
-		'message' => $message,
-		'urls' => $urls,
-	)
-);
+echo json_encode('Ok');
 exit;
-
-?>
