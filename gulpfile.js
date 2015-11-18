8598d3839b36dde623cc323b49d9dc84382dfa9f
Aaron Burtnyk <burt202@gmail.com>
2015-11-18 21:06:30 +0000

    Swap npm scripts for gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var mocha = require("gulp-mocha");
+var istanbul = require("gulp-istanbul");
+
+var PATH_TO_SRC = "jquery.liteuploader.js";
+var MINIFIED_FILE_NAME = "jquery.liteuploader.min.js";
+
+gulp.task("default", ["watch", "tests"]);
+
+gulp.task("watch", function () {
+  gulp.watch(PATH_TO_SRC, ["minify"]);
+});
+
+gulp.task("minify", function() {
+  return gulp.src(PATH_TO_SRC)
+    .pipe(uglify({preserveComments: "license"}))
+    .pipe(rename(MINIFIED_FILE_NAME))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("coverage", function () {
+  return gulp.src(PATH_TO_SRC)
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task("tests", ["coverage"], function () {
+  return gulp.src(["test/*.js"])
+    .pipe(mocha())
+    .pipe(istanbul.writeReports())
+    .on("finish", function () {
+      console.log("Breakdown: file://" + __dirname + "/coverage/index.html");
+    });
+});
